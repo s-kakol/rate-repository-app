40277801325515fee57081e842b47dc22b46e500
s-kakol <kakolsylwester@gmail.com>
2022-07-28 16:59:17 +0200

    Feat: Finalize RepositoryItem styling

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,13 +1,41 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+import Theme from '../theme';
 import RepositoryItemDetail from './RepositoryItemDetail';
 import Text from './Text';
 
 const styles = StyleSheet.create({
   container: {
+    padding: 16,
     backgroundColor: 'white',
+  },
+  mainArea: {
+    paddingLeft: 12,
+    marginRight: 90,
+  },
+  detailsArea: {
+    marginTop: 16,
+    padding: 4,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
   },
   row: {
     flexDirection: 'row',
+  },
+  image: {
+    paddingRight: 12,
+    width: 64,
+    height: 64,
+    borderRadius: 12,
+  },
+  descriptionBar: {
+    paddingTop: 6,
+    paddingBottom: 6,
+  },
+  languageBar: {
+    alignSelf: 'flex-start',
+    padding: 4,
+    borderRadius: 4,
+    backgroundColor: Theme.colors.primary,
   },
 });
 
@@ -15,14 +43,20 @@ const RepositoryItem = ({ repo }) => {
   return (
     <View style={styles.container}>
       <View style={styles.row}>
-        <Text>IMG</Text>
-        <View>
-          <Text>{repo.fullName}</Text>
-          <Text>{repo.description}</Text>
-          <Text>{repo.language}</Text>
+        <Image style={styles.image} source={{ uri: repo.ownerAvatarUrl }} />
+        <View style={styles.mainArea}>
+          <Text fontSize="subheading" fontWeight="bold">
+            {repo.fullName}
+          </Text>
+          <Text style={styles.descriptionBar} color="textSecondary">
+            {repo.description}
+          </Text>
+          <Text style={styles.languageBar} color="textWhite">
+            {repo.language}
+          </Text>
         </View>
       </View>
-      <View style={styles.row}>
+      <View style={styles.detailsArea}>
         <RepositoryItemDetail value={repo.stargazersCount} name="Stars" />
         <RepositoryItemDetail value={repo.forksCount} name="Forks" />
         <RepositoryItemDetail value={repo.reviewCount} name="Reviews" />
--- src/components/RepositoryItemDetail.jsx
+++ src/components/RepositoryItemDetail.jsx
@@ -2,10 +2,15 @@ import { View } from 'react-native';
 import Text from './Text';
 
 const RepositoryItemDetail = props => {
+  const handleBigValue = number => `${Math.round(number / 100) / 10}k`;
+
+  const displayValue =
+    props.value >= 1000 ? handleBigValue(props.value) : props.value;
+
   return (
-    <View>
-      <Text>{props.value}</Text>
-      <Text>{props.name}</Text>
+    <View style={{ alignItems: 'center' }}>
+      <Text fontWeight="bold">{displayValue}</Text>
+      <Text color="textSecondary">{props.name}</Text>
     </View>
   );
 };
