3dcb4b80b95eef5ed3d04c17e1b3c862d5a396b1
s-kakol <kakolsylwester@gmail.com>
2022-07-28 17:42:18 +0200

    Feat: Add current routes in Main.js

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,8 @@
 import { StyleSheet, View } from 'react-native';
+import { Route, Routes, Navigate } from 'react-router-native';
 import AppBar from './components/AppBar';
 import RepositoryList from './components/RepositoryList';
+import SignIn from './components/SignIn';
 import Theme from './theme';
 
 const styles = StyleSheet.create({
@@ -15,7 +17,11 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} exact />
+        <Route path="/login" element={<SignIn />} exact />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </View>
   );
 };
