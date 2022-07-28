21e0fba53c055e3a1769f8dfe56f72196d3ccc4d
s-kakol <kakolsylwester@gmail.com>
2022-07-28 18:36:07 +0200

    Feat: Update SignIn with formik inputs

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,37 @@
+import { Formik } from 'formik';
+import { Pressable, View } from 'react-native';
+import FormikTextInput from './FormikTextInput';
+
 import Text from './Text';
 
+const initialValues = {
+  login: '',
+  password: '',
+};
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View>
+      <FormikTextInput name="login" placeholder="Username" />
+      <FormikTextInput name="password" placeholder="Password" />
+      <Pressable onPress={onSubmit}>
+        <Text>Login</Text>
+      </Pressable>
+    </View>
+  );
+};
+
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  const onSubmit = values => {
+    console.log(`Login: ${values.login}`);
+    console.log(`Pass: ${values.password}`);
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
