76fe6277bb8b9d50c2555ea89d3e1459f0fa907c
s-kakol <kakolsylwester@gmail.com>
2022-08-01 14:12:35 +0200

    Fix: Fix typo

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: 'http://192.168.1.10:4000/grahpql',
+  uri: 'http://192.168.1.10:4000/graphql',
 });
 
 const createApolloClient = () => {
