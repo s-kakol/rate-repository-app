df45c545a8dea55bc0cf44a79e76143268371505
s-kakol <kakolsylwester@gmail.com>
2022-08-02 22:03:55 +0200

    Feat: Implement storage for storing auth token

---
+++ src/utils/authStorage.js
@@ -0,0 +1,22 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+class AuthStorage {
+  constructor(namespace = 'auth') {
+    this.namespace = namespace;
+  }
+
+  async getAccessToken() {
+    return await AsyncStorage.getItem(`${this.namespace}:token`);
+  }
+
+  async setAccessToken(accessToken) {
+    this.removeAccessToken();
+    await AsyncStorage.setItem(`${this.namespace}:token`, accessToken);
+  }
+
+  async removeAccessToken() {
+    await AsyncStorage.removeItem(`${this.namespace}:token`);
+  }
+}
+
+export default AuthStorage;
