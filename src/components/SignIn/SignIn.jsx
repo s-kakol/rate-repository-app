3abc03f790db966011a49b75e8444f4f26f919f8
s-kakol <kakolsylwester@gmail.com>
2022-08-03 14:32:49 +0200

    Refactor: Move SignIn to seperate directory
    Also move SignInForm to seperate file

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,27 +1,10 @@
 import { Formik } from 'formik';
-import { Pressable, View, StyleSheet } from 'react-native';
 import * as yup from 'yup';
 import { useNavigate } from 'react-router-native';
 import { useApolloClient } from '@apollo/client';
 
-import FormikTextInput from './FormikTextInput';
-import Text from './Text';
-import Theme from '../theme';
-import useSignIn from '../hooks/useSignIn';
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 8,
-  },
-  button: {
-    backgroundColor: Theme.colors.primary,
-    padding: 12,
-    margin: 8,
-    borderRadius: 4,
-    alignItems: 'center',
-  },
-});
+import useSignIn from '../../hooks/useSignIn';
+import SignInForm from './SignInForm';
 
 const initialValues = {
   username: '',
@@ -38,24 +21,6 @@ const validationSchema = yup.object().shape({
     .min(3, 'Password must be at least 3 characters long')
     .required('Please enter your password'),
 });
-
-const SignInForm = ({ onSubmit }) => {
-  return (
-    <View style={styles.container}>
-      <FormikTextInput name="username" placeholder="Username" />
-      <FormikTextInput
-        name="password"
-        placeholder="Password"
-        secureTextEntry={true}
-      />
-      <Pressable onPress={onSubmit} style={styles.button}>
-        <Text color="textWhite" fontWeight="bold">
-          Sign in
-        </Text>
-      </Pressable>
-    </View>
-  );
-};
 
 const SignIn = () => {
   const [signIn] = useSignIn();
