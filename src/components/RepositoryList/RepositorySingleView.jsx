48326e9b791566ff1fada9a9b9400a8f4bd8df42
s-kakol <kakolsylwester@gmail.com>
2022-08-08 11:17:03 +0200

    Feat(SingleView): Render reviews list

--- src/components/RepositoryList/RepositorySingleView.jsx
+++ src/components/RepositoryList/RepositorySingleView.jsx
@@ -1,15 +1,35 @@
+import { View, FlatList, StyleSheet } from 'react-native';
 import { useParams } from 'react-router-native';
 
 import useRepository from '../../hooks/useRepository';
 import RepositoryItem from './RepositoryItem';
 import LoadingIndicator from '../LoadingIndicator';
+import ReviewItem from './ReviewItem';
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+});
+
+const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositorySingleView = () => {
   const repositoryId = useParams().id;
   const { repository } = useRepository(repositoryId);
+  const reviewNodes = repository
+    ? repository.reviews.edges.map(edge => edge.node)
+    : [];
 
   return repository ? (
-    <RepositoryItem singleView={true} repo={repository} />
+    <FlatList
+      data={reviewNodes}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={({ item }) => <ReviewItem review={item} />}
+      ListHeaderComponent={() => (
+        <RepositoryItem singleView={true} repo={repository} />
+      )}
+    />
   ) : (
     <LoadingIndicator />
   );
---
+++ src/components/RepositoryList/ReviewItem.jsx
@@ -0,0 +1,53 @@
+import { View, StyleSheet } from 'react-native';
+
+import Text from '../Text';
+import theme from '../../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 8,
+    padding: 16,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+  },
+  rating: {
+    padding: 8,
+    borderWidth: 2,
+    borderColor: theme.colors.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 48,
+    width: 48,
+    borderRadius: 48 / 2,
+  },
+  content: {
+    paddingLeft: 16,
+    paddingRight: 48,
+  },
+  text: {
+    paddingTop: 4,
+    textAlign: 'justify',
+  },
+});
+
+const ReviewItem = ({ review }) => {
+  const date = new Date(review.createdAt).toLocaleDateString();
+  return (
+    <View style={styles.container}>
+      <View style={styles.rating}>
+        <Text color="primary" fontWeight="bold" fontSize="subheading">
+          {review.rating}
+        </Text>
+      </View>
+      <View style={styles.content}>
+        <Text fontWeight="bold" fontSize="subheading">
+          {review.user.username}
+        </Text>
+        <Text color="textSecondary">{date}</Text>
+        <Text style={styles.text}>{review.text}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default ReviewItem;
