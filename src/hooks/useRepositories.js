aeb4cd1d7b75949fab64c0d17a51a1aeddce3b9b
s-kakol <kakolsylwester@gmail.com>
2022-08-15 16:39:04 +0200

    Style: Remove unnecessary, commented code

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,14 +1,7 @@
-// import { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = variables => {
-  // const [repositories, setRepositories] = useState();
-  // const { data, error, loading } = useQuery(GET_REPOSITORIES, {
-  //   fetchPolicy: 'cache-and-network',
-  //   variables,
-  // });
-
   const { data, loading, fetchMore, ...result } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
     variables,
@@ -28,14 +21,6 @@ const useRepositories = variables => {
       },
     });
   };
-
-  // useEffect(() => {
-  //   if (!error && !loading) {
-  //     setRepositories(data.repositories);
-  //   }
-  // }, [error, loading]);
-
-  // return { repositories };
 
   return {
     repositories: data?.repositories,
