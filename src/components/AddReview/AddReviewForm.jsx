ce96dfd6ddf46cd81f1c4c5bea11e2e441847caf
s-kakol <kakolsylwester@gmail.com>
2022-08-18 20:14:00 +0200

    Fix(AddReviewForm): Make review input multiline

--- src/components/AddReview/AddReviewForm.jsx
+++ src/components/AddReview/AddReviewForm.jsx
@@ -10,7 +10,7 @@ const AddReviewForm = ({ onSubmit }) => {
       <FormikTextInput name="ownerName" placeholder="Repository owner name" />
       <FormikTextInput name="repositoryName" placeholder="Repository name" />
       <FormikTextInput name="rating" placeholder="Rating between 0 and 100" />
-      <FormikTextInput name="text" placeholder="Review" />
+      <FormikTextInput name="text" placeholder="Review" multiline />
       <Pressable onPress={onSubmit} style={FormsStyle.button}>
         <Text color="textWhite" fontWeight="bold">
           Add review
