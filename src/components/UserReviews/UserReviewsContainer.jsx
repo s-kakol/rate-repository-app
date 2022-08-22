679eeaf185bdbae6c628697310c016bb98f62386
s-kakol <kakolsylwester@gmail.com>
2022-08-22 22:02:49 +0200

    Feat: Add view for users without reviews

--- src/components/UserReviews/UserReviewsContainer.jsx
+++ src/components/UserReviews/UserReviewsContainer.jsx
@@ -1,16 +1,35 @@
-import { FlatList, View, StyleSheet } from 'react-native';
+import { FlatList, View, StyleSheet, Pressable } from 'react-native';
+import { useNavigate } from 'react-router-native';
+
 import ReviewItem from '../ReviewItem';
 import Text from '../Text';
+import Theme from '../../theme';
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 32,
+    alignItems: 'center',
+  },
   separator: {
     height: 10,
+  },
+  button: {
+    backgroundColor: Theme.colors.primary,
+    padding: 16,
+    margin: 24,
+    borderRadius: 6,
   },
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const UserReviewsContainer = ({ user }) => {
+  const navigate = useNavigate();
+  const redirectToAddReviewForm = () => {
+    navigate('/add-review');
+  };
+
   const reviewNodes =
     user.reviews.edges.length > 0
       ? user.reviews.edges.map(edge => edge.node)
@@ -25,9 +44,16 @@ const UserReviewsContainer = ({ user }) => {
       )}
     />
   ) : (
-    //To add later: Button to redirect user to "Add review" page
-    //user: test1
-    <Text>Add your first review!</Text>
+    <View style={styles.container}>
+      <Text color="textSecondary" fontSize="subheading" fontWeight="bold">
+        You haven&apos;t added any reviews yet...
+      </Text>
+      <Pressable style={styles.button} onPress={redirectToAddReviewForm}>
+        <Text color="textWhite" fontWeight="bold">
+          Add your first review
+        </Text>
+      </Pressable>
+    </View>
   );
 };
 
